ccf732c6bcebd3e62ffe859cff9d21db84cf68e7
charmslcd <137902588+charmslcd@users.noreply.github.com>
2025-01-31 17:50:15 +0800

    Add files via upload
    Signed-off-by: charmslcd <137902588+charmslcd@users.noreply.github.com>

--- day-25-us-states-game/us-states-game/main.py
+++ day-25-us-states-game/us-states-game/main.py
@@ -11,7 +11,7 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 is_game_on = True
-correct_answers = []
+guessed_states = []
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
@@ -24,17 +24,29 @@ def write_state(name, xcor, ycor):
     state.write(f"{name}", align=ALIGNMENT, font=FONT)
 
 
-while is_game_on:
-    answer_state = screen.textinput(title=f"{len(correct_answers)}/50 Guess the state",
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 correct states",
                                     prompt="What's another state name?")
-    new_state_title = answer_state.title()
 
-    if new_state_title in all_states and new_state_title not in correct_answers:
-        correct_answers.append(new_state_title)
+    if answer_state == "Exit":
+        states_to_learn_list = []
+        for state in all_states:
+            if state not in guessed_states:
+                states_to_learn_list.append(state)
+
+        states_to_learn = pd.DataFrame(states_to_learn_list, columns=["state"])
+        states_to_learn.to_csv("states_to_learn.csv")
+        break
+
+    new_state_title = answer_state.title()
+    if new_state_title in all_states and new_state_title not in guessed_states:
+        guessed_states.append(new_state_title)
         state_data = data[data.state == new_state_title]
         state_name = new_state_title
         state_xcor = state_data.x.iloc[0]
         state_ycor = state_data.y.iloc[0]
         write_state(state_name, state_xcor, state_ycor)
-        print(correct_answers)
+        print(guessed_states)
 
+
+# states_to_learn.csv
