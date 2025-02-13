ef8a3ff8e8756d2705180f0acf5c6c41eb123533
charmslcd <137902588+charmslcd@users.noreply.github.com>
2025-02-13 11:12:29 +0800

    Add files via upload
    Signed-off-by: charmslcd <137902588+charmslcd@users.noreply.github.com>

--- day-26-nato-phonetic/nato-phonetic-alphabet/main.py
+++ day-26-nato-phonetic/nato-phonetic-alphabet/main.py
@@ -1,17 +1,23 @@
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
 import pandas as pd
+from math import log
 
 nato = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter a word: ").upper()
-result_list = [nato_dict[letter] for letter in user_input]
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        result_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result_list)
 
-print(result_list)
+
+generate_phonetic()
+# result_list = [catch(log, letter) for letter in user_input]
 
 
 
